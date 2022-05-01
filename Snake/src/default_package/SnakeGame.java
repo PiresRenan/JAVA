6665fc758fa24c5a220a5c82957a1e79e9c51d33
PiresRenan <84561423+PiresRenan@users.noreply.github.com>
2022-04-30 22:16:05 -0300

    Add files via upload

---
+++ Snake/src/default_package/SnakeGame.java
@@ -0,0 +1,11 @@
+package default_package;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		new GameFrame();
+
+	}
+
+}
---
+++ TicTacToe/src/VelhaGame/Main.java
@@ -0,0 +1,11 @@
+package VelhaGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		TicTacToe tictactoe = new TicTacToe();
+
+	}
+
+}
